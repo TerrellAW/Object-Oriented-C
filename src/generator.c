de7082cc8e70c08369d087dcb5ee36de9f64cb83
TerrellAW <terraw1999@proton.me>
2025-12-13 20:13:55 -0700

    Add: Identifier/variable handling

--- src/generator.c
+++ src/generator.c
@@ -21,13 +21,30 @@ Generator gen_create(NodeMain root) {
 	return gen;
 }
 
-void gen_expr(const NodeExpr expr, const char* outputname, size_t* out_size) {
+void gen_expr(Generator* generator, const NodeExpr expr, const char* outputname, size_t* out_size) {
+	char* asm_stmt = "";
 	switch (expr.type) {
 		case expr_int:
 			write_push(outputname, expr.token.value, out_size); // Push value of integer onto stack
 			break;
 		case expr_idnt:
-			exit(EXIT_FAILURE); // TODO actually deal with identifiers
+			// Find matching variable
+			Var var;
+			for (size_t i = 0; i < generator->var_count; i++) {
+				if (strcmp(expr.token.value, generator->vars[i].name) == 0) {
+					var = generator->vars[i];
+					break;
+				}
+			}
+			// Handle undeclared variable
+			if (var.name == NULL) {
+				fprintf(stderr, "Compilation Error: Undeclared variable %s", expr.token.value);
+				exit(EXIT_FAILURE);
+			}
+			// Get stack offset for variable
+			sprintf(asm_stmt, "[rsp+%zu]", generator->stack_size - var.stack_loc);
+			write_push(outputname, asm_stmt, out_size);
+			break;
 		default:
 			exit(EXIT_FAILURE); // TODO error handling
 	}
@@ -36,23 +53,22 @@ void gen_expr(const NodeExpr expr, const char* outputname, size_t* out_size) {
 void gen_stmt(Generator* generator, const NodeStmt stmt, const char* outputname, size_t* out_size) {
 	switch (stmt.type) {
 		case stmt_exit:
-			gen_expr(stmt.expr, outputname, out_size); // Push variable onto stack
+			gen_expr(generator, stmt.expr, outputname, out_size); // Push variable onto stack
 			write_exit_from_stack(outputname, out_size); // Pop value from stack for exit code
 			generator->has_exit = true;
 			break;
 		case stmt_ret:
 			// TODO: Have return not exit, but return
-			gen_expr(stmt.expr, outputname, out_size);
+			gen_expr(generator, stmt.expr, outputname, out_size);
 			write_exit_from_stack(outputname, out_size);
 			generator->has_exit = true;
 			break;
 		case stmt_type:
 			for (size_t i = 0; i < generator->var_count; i++) {
 				if (strcmp(stmt.var.node_type.idnt.value, generator->vars[i].name)) {
-					fprintf(stderr, "Compiler Error: Identifier already used %s\n", generator->vars[i].name);
+					fprintf(stderr, "Compilation Error: Identifier already used %s\n", generator->vars[i].name);
 					exit(EXIT_FAILURE);
 				}
-
 				// Add to generator
 				Var var; // Initialize variable struct
 				var.name = stmt.var.node_type.idnt.value; // Get name from identifier
@@ -67,11 +83,9 @@ void gen_stmt(Generator* generator, const NodeStmt stmt, const char* outputname,
 						&generator->vars[generator->var_count - 1].name);
 				
 				// Evaluate expression
-				gen_expr(stmt.expr, outputname, out_size); // Push expression onto stack
-
-				// TODO: Push into system stack
-				
+				gen_expr(generator, stmt.expr, outputname, out_size); // Push expression onto stack
 			}
+			break;
 		default:
 			exit(EXIT_FAILURE); // TODO error handling
 	}
