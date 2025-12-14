00c93537bee1eb2c033da5e5875a459bc0336ddf
TerrellAW <terraw1999@proton.me>
2025-12-14 15:20:09 -0700

    Add: Detailed documentation for hashing api

--- include/hash.h
+++ include/hash.h
@@ -6,6 +6,17 @@
 
 /**
  * @brief Generates a simple hash code for a word.
+ *
+ * Uses a simple formula to convert a string into an integer using
+ * the string's length, the ASCII code of its first character
+ * and the ASCII code of its last character.
+ * The formula is:
+ * length * 100 + (word[0] - 'a') * 10 + (word[length - 1] - 'a')
+ *
+ * @param word String of characters to hash into an integer.
+ * @param length Size of the string, used in hashing calculation.
+ *
+ * @return Hash code for the given string, or 0 if length is 0.
  */
 int gen_word_hash(const char* word, size_t length);
 
